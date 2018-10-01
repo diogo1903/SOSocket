127663ca2c0f85741164e36cc9b8222cb7c29cb4
alesouzarosa <43153497+alesouzarosa@users.noreply.github.com>
2018-10-01 01:41:35 -0300

    Update servidor6.c

--- servidor6.c
+++ servidor6.c
@@ -18,14 +18,18 @@ typedef struct pedido{
     char texto2[64];
 }pedido;
 
-
-
 void saidals(char* resposta)
 {
     FILE *fp;
     char path[1035];
     char c;
     char total[10000];
+    for(int i=0; i < 1035;i++)
+        path[i] = 0;
+
+    for(int i=0; i < 10000;i++)
+        total[i] = 0;
+
     fp = popen("ls *", "r");
     if (fp == NULL)
     {
@@ -67,43 +71,35 @@ void saidaler(char* resposta, pedido pedido1)
     strcpy(resposta, total);
 }
 
-
-
-
-void limpar_pedido(pedido pedido1)
+pedido limpar_pedido(pedido pedido1)
 {   
     for(int i =0 ; i < 16; i++)
-    pedido1.comando[i] = NULL;
+    pedido1.comando[i] = 0;
     for(int j =0 ; j < 64 ; j++)
-    pedido1.texto[j] = NULL;
+    pedido1.texto[j] = 0;
     for(int j =0 ; j < 64 ; j++)
-    pedido1.texto2[j] = NULL;
+    pedido1.texto2[j] = 0;
 }
-
 
 pedido trat_leitura(char msg[80])
 {
     pedido pedido1;
     int i = 0, teste = 0, j=0;
     int ultimo = strlen(msg);
-    msg[ultimo-1] = NULL;
+    msg[ultimo-1] = 0;
     
-    char * pch;
+    char *pch;
     pch = strtok (msg," ");
     if(pch != NULL){
         strcpy(pedido1.comando, pch);
         pch = strtok (NULL, " ");
-    }
-      if(pch != NULL){
+    }if(pch != NULL){
         strcpy(pedido1.texto, pch);
         pch = strtok (NULL, " ");
-    }
-    if(pch != NULL){
+    }if(pch != NULL){
         strcpy(pedido1.texto2, pch);
         pch = strtok (NULL, " ");
     }
-    
-    
     return pedido1;
 }
 
@@ -112,76 +108,77 @@ void *conexao(void *connfd)
     int connfd2 = *(int*)connfd;
     char msg[80];
     pedido pedido1;
-    char resposta[1000000];
+    char resposta[1000];
     char sendBuff[1025];
     while(1)
     {
-    //apaga mensagem
-    void limpar_pedido(pedido1);
-    for(int i=0; i<= sizeof(msg);i++)
-        msg[i] = NULL;
-    printf("accept ok \n"); 
+        //apaga mensagem
+        pedido1 = limpar_pedido(pedido1);
+        for(int i=0; i<  80;i++)
+            msg[i] = 0;
+        for(int i=0; i< 1000 ;i++)
+            resposta[i] = 0;
+        for(int i=0; i< 1025 ;i++)
+            sendBuff[i] = 0;
 
-    if(read(connfd2, msg, sizeof(msg))<0)
-    {
-        printf("Error de leitura do socket\n");
-        exit(0);
-    }        
-    
-    pedido1 = trat_leitura(msg);
+        printf("accept ok \n"); 
 
-    printf("**********testando mensagem*************\n");
-    printf("mensagem: %s \n", msg);
-    printf("comando:%s tamanho :%d\n", pedido1.comando, strlen(pedido1.comando));
-    printf("texto:%s tamanho :%d\n", pedido1.texto, strlen(pedido1.texto));
-    printf("texto2:%s tamanho :%d\n", pedido1.texto2, strlen(pedido1.texto2));
-    char temp[40];
-    if(strcmp("crdir", pedido1.comando)==0){
-        strcpy(resposta, "novo dir");
-        strcpy(temp, "mkdir " );
-        strcat(temp, pedido1.texto);
+        if(read(connfd2, msg, sizeof(msg))<0)
+        {
+            printf("Error de leitura do socket\n");
+            exit(0);
+        }        
         
-        system(temp);
-    }else if(strcmp("rm", pedido1.comando)==0){
-        strcpy(resposta, "deletado");
-        strcpy(temp, "rm -r ");
-        strcat(temp, pedido1.texto);
-        system(temp);
-    }else if(strcmp("sair", pedido1.comando)==0){
-        strcpy(resposta, "saindo");
-        close(connfd2);
-        //exit(1);
-    }else if(strcmp("listar", pedido1.comando)==0){
-        saidals(resposta);
-    }else if(strcmp("crarq", pedido1.comando)==0){
-        strcpy(resposta, "arquivo criado");
-        strcpy(temp, "touch " );
-        strcat(temp, pedido1.texto);
-        system(temp);
-    }else if(strcmp("escrever", pedido1.comando)==0){
-        strcpy(resposta, "escrito");
-        strcpy(temp, "echo \"" );
-        strcat(temp, pedido1.texto);
-        strcat(temp, "\" > ");
-        strcat(temp, pedido1.texto2);
-        system(temp);
-    }else if(strcmp("ler", pedido1.comando)==0){
-        saidaler(resposta, pedido1);
-    }else if(strcmp("cd", pedido1.comando)==0){
-        strcpy(resposta, "diretorio escolhido");
-        strcpy(temp, "cd " );
-        strcat(temp, pedido1.texto);
-        system(temp);
-    }else{
-        strcpy(resposta, "comando nao exite\n");
-    }
+        pedido1 = trat_leitura(msg);
 
-    snprintf(sendBuff, sizeof(sendBuff), "%s\n",(char*)&resposta );
-    write(connfd2, sendBuff,strlen(sendBuff));
-    printf("teste 1\n");
-    //close(connfd2);
-    
-    printf("teste 1\n");
+        printf("**********testando mensagem*************\n");
+        printf("mensagem: %s \n", msg);
+        printf("comando:%s tamanho :%d\n", pedido1.comando, strlen(pedido1.comando));
+        printf("texto:%s tamanho :%d\n", pedido1.texto, strlen(pedido1.texto));
+        printf("texto2:%s tamanho :%d\n", pedido1.texto2, strlen(pedido1.texto2));
+        char temp[40];
+        if(strcmp("crdir", pedido1.comando)==0){
+            strcpy(resposta, "novo dir");
+            strcpy(temp, "mkdir " );
+            strcat(temp, pedido1.texto);            
+            system(temp);
+        }else if(strcmp("rm", pedido1.comando)==0){
+            strcpy(resposta, "deletado");
+            strcpy(temp, "rm -r ");
+            strcat(temp, pedido1.texto);
+            system(temp);
+        }else if(strcmp("sair", pedido1.comando)==0){
+            strcpy(resposta, "saindo");
+            //close(connfd2);
+            return 0;
+        }else if(strcmp("listar", pedido1.comando)==0){
+            saidals(resposta);
+        }else if(strcmp("crarq", pedido1.comando)==0){
+            strcpy(resposta, "arquivo criado");
+            strcpy(temp, "touch " );
+            strcat(temp, pedido1.texto);
+            system(temp);
+        }else if(strcmp("escrever", pedido1.comando)==0){
+            strcpy(resposta, "escrito");
+            strcpy(temp, "echo \"" );
+            strcat(temp, pedido1.texto);
+            strcat(temp, "\" > ");
+            strcat(temp, pedido1.texto2);
+            system(temp);
+        }else if(strcmp("ler", pedido1.comando)==0){
+            saidaler(resposta, pedido1);
+        }else if(strcmp("cd", pedido1.comando)==0){
+            strcpy(resposta, "diretorio escolhido");
+            strcpy(temp, "cd " );
+            strcat(temp, pedido1.texto);
+            system(temp);
+        }else{
+            strcpy(resposta, "comando nao exite\n");
+        }
+
+        snprintf(sendBuff, sizeof(sendBuff), "%s\n",(char*)&resposta );
+        write(connfd2, sendBuff,strlen(sendBuff));
+        //close(connfd2);
     }
     free(connfd);
     return 0;
@@ -192,8 +189,8 @@ void *conexao(void *connfd)
 int main(int argc, char * argv[])
 {
     int listenfd=0, connfd=0, *novo_sock;
-    struct sockaddr_in serv_addr;// é um estrutura do sockaddr
-    struct sockaddr_in sock_cli; // é o socket do cliente 
+    struct sockaddr_in serv_addr;// Ã© um estrutura do sockaddr
+    struct sockaddr_in sock_cli; // Ã© o socket do cliente 
     
     socklen_t sock_novo_len; 
     
@@ -235,6 +232,5 @@ int main(int argc, char * argv[])
     }
     
 }
-
 
 
