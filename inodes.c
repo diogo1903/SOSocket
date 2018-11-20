fdeb7db30807ec0795b3c63d6a618cc220f62961
diogo1903 <43153528+diogo1903@users.noreply.github.com>
2018-11-20 18:26:30 -0200

    Update inodes.c

--- inodes.c
+++ inodes.c
@@ -3,21 +3,21 @@
 #include <string.h>
 
 /*
-listagem de 064 blocos =   16 bytes - bytes 0 a 15
-  10 inodes * 76 bytes =  760 bytes - bytes 15 a 775
-   64 blocos * 4 bytes =  256 bytes - bytes 776 a 1023
-                 total = 1024 bytes
+listagem de 064 blocos =   16 bytes - bytes 0 a 16
+  10 inodes * 76 bytes =  760 bytes - bytes 8 a 775
+   64 blocos * 4 bytes =  256 bytes - bytes 776 a 1031
+                 total = 1032 bytes
 */
 
 #define taminodes 76
 #define tambloco 4
-#define nbytelistabits 8
+#define nlistabits 8
 #define ninodes 10
 #define nblocos 64
 #define iniclista 0
 #define inicinodes 16
 #define inicblocos 776
-#define tambuffer 1024
+#define tambuffer 1032
 
 typedef struct nos{
   short tipo; //tipo do arquivo 0 - livre, 1 - diretorio, 2 - ".txt"
@@ -29,7 +29,7 @@ typedef struct nos{
 }inode;
 
 typedef struct arq{
-  char livre[nbytelistabits];
+  short livre[nlistabits];
   inode i[ninodes];
   char blocos[nblocos*tambloco];
 }arquivo;
@@ -37,34 +37,70 @@ typedef struct arq{
 FILE *fp;
 arquivo principal;
 
-
-int alocaespaco(char texto){
-
+// Calcula posição na memoria, retorna posição. Recebe x = arquivo.livre[x] e y = slot (1 e 2)
+int conta(int x, int pos){ // pega end bloco
+  int resultado;
+  resultado = ((pos-1) * 4 + x * 8) + inicblocos;
+  return resultado;
 }
 
-void printinode(inode a){
-  int x;
-  printf("Tipo: %c\n", a.tipo);
-  printf("Tamanho: %d Bytes\n",a.tam);
-  printf("Tam Lista: %c \nLista: ", a.tamlista);
-  for(x = 0; x < 9; x++){
-    printf("%d, ", a.lista[x]);
+// Procura slot de 4 bytes livres, retorna end do primeiro
+int alocaespaco(arquivo *novo){
+  int x, res;
+  for(x = 0; x < nlistabits; x++){
+    if(novo->livre[x] = 0){ // sem espaço ocupado
+      novo->livre[x] = 1;
+      res = conta(x, 1);
+      return res;
+    }
+    if(novo->livre[x] = 1){ //pega segundo slot
+      novo->livre[x] = 3;
+      res = conta(x, 2);
+      return res;
+    }
+    if(novo->livre[x] = 2){ //pega primeiro slot
+      novo->livre[x] = 3;
+      res = conta(x, 1);
+      return res;
+    }
   }
-  printf("%d\n", a.tamnome);
+  printf("Sem espaço");
+  return -1;
+}
+
+//define inode, iniciando com nome e tipo, retorna posição do inode na lista
+int criainode(arquivo *novo, int tipo, char nome[50]){ 
+  int x, y;
+  for(x = 0; x < ninodes; x++){
+    if(novo->i[x].tipo = 0){
+      novo->i[x]->tipo = tipo;
+      strcpy(novo->i[x]->nome, nome);
+      novo->i[x]->tam = 0;
+      novo->i[x]->tamlista = 0;
+      for(y = 0; y < 9; y++){
+        novo->i[x]->lista[y] = 0;
+      }
+      novo->i[x]->tamnome = strlen(nome);
+    }
+    return x;
+  }
+  if(x >= ninodes){
+    printf("Sem Inode disponivel!\n");
+  }
+}
+
+void printinode(inode *a){
+  int x;
+  printf("Tipo: %d\n", a->tipo);
+  printf("Tamanho: %d Bytes\n",a->tam);
+  printf("Tam Lista: %d \nLista: ", a->tamlista);
+  for(x = 0; x < 9; x++){
+    printf("%d, ", a->lista[x]);
+  }
+  printf("%d\n", a->tamnome);
   printf("\n Nome: ");
   for(x = 0; x < 50; x++){
-    printf("%c", a.nome[x]);
+    printf("%c", a->nome[x]);
   }
   printf("\n");
 }
-
-int main(){
-
-  int x = sizeof(inode);
-  int y = sizeof(arquivo);
-  printf("texto texto texto\n\n");
-  printf("%d \n%d", x, y);
-  return 0;
-}
-
-// int fseek (FILE *fp, long numbytes, int origem);
